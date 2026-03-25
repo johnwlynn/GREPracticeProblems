dabf3e8b283c4ab26dca3691b6cac5c5cda71b88
John Lynn <johnlynn@gmail.com>
2026-03-25 12:50:49 -0600

    Add files via upload

---
+++ grePracticeProblems.py
@@ -0,0 +1,35 @@
+#GRE Quant Practice Problems - https://www.youtube.com/watch?v=ZvdXkDUaavc
+
+
+#how many positive integers less than 200 are divisible by 4 or 5
+
+# 2 lines of code answer
+blab = lambda x: x%4==0 or x%5==0
+arrayOfFourOrFive = [item for item in range(1,200) if blab(item)]
+print (len(arrayOfFourOrFive))
+
+
+#1 line of code answer 
+blah = filter(lambda x: x%4==0 or x%5==0, range(1,200))
+print(len(list(blah))) #got in the weeds a little on the one line solution so did the two liner then was able to figure out the 1 line easily
+
+
+
+
+
+
+
+# If x is an integer less than 10, how many different triangles are possible
+#
+#                  ,
+#                 / \
+#                /   \
+#               /     \
+#          x   /       \   x-2
+#             /         \
+#            /           \
+#           /             \
+#           ```````````````
+#                  5
+#
+#
